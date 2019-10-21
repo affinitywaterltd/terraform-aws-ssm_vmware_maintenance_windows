f31a412831523a19aec21d118ef3ff855c7c9921
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-10-21 11:41:30 +0100

    Add Powershell filecopy

--- main.tf
+++ main.tf
@@ -90,6 +90,43 @@ resource "aws_ssm_maintenance_window_task" "default_task_snapshot" {
   }
 }
 
+resource "aws_ssm_maintenance_window_task" "default_copy_aws_update_tools" {
+  count            = "${var.weeks}"
+  window_id        = "${element(aws_ssm_maintenance_window.default.*.id, count.index)}"
+  name             = "copy_aws_update_tools"
+  description      = "Copy AWS Patching tools from local share"
+  task_type        = "RUN_COMMAND"
+  task_arn         = "AWS-RunPowerShellScript"
+  priority         = 25
+  service_role_arn = "${var.role}"
+  max_concurrency  = "${var.mw_concurrency}"
+  max_errors       = "${var.mw_error_rate}"
+
+  targets {
+    key    = "WindowTargetIds"
+    values = ["${element(aws_ssm_maintenance_window_target.default.*.id, count.index)}"]
+  }
+
+  task_invocation_parameters {
+    run_command_parameters {
+      output_s3_bucket = "${var.s3_bucket}"
+      output_s3_key_prefix = "${var.weeks > 1 ? "${var.type}_week-${count.index+1}_${var.day}_${var.hour}00/${var.account}-${var.environment}" : "${var.type}_week-${var.week}_${var.day}_${var.hour}00/${var.account}-${var.environment}" }"
+      service_role_arn = "${var.role}"
+      timeout_seconds  = 300
+
+      parameter {
+        name   = "commands"
+        values = ["$remoteBase = '\\\\' + $(Get-WmiObject Win32_ComputerSystem).Domain + '\\NETLOGON\\SSM_Scripts\\'", "$zipFilename = 'AWSUpdateWindowsInstance_1_4_4_0_customised.zip'", "$moduleName = 'AWSUpdateWindowsInstance'", "$tempPath = $env:TEMP", "$moduleDirectory = Join-Path $tempPath -ChildPath $moduleName", "$moduleZipFilePath = Join-Path $tempPath -ChildPath $zipFilename", "$remoteFilePath = Join-Path $remoteBase -ChildPath $zipFilename", "Copy-Item -Path $remoteFilePath -Destination $moduleZipFilePath -Confirm:$false"]
+      }
+      parameter {
+        name   = "executionTimeout"
+        values = ["300"]
+      }
+    }
+  }
+}
+
+
 resource "aws_ssm_maintenance_window_task" "default_task_updates" {
   count            = "${var.weeks}"
   window_id        = "${element(aws_ssm_maintenance_window.default.*.id, count.index)}"
